5cd0bc34e119c061b79e1bb7d4fe2af8d81caee6
Habibur Rahman <habib.swi16@gmail.com>
2023-08-27 19:37:03 +0600

    phone card

---
+++ main.js
@@ -0,0 +1,40 @@
+const loadPhone = async () => {
+  const res = await fetch(
+    "https://openapi.programming-hero.com/api/phones?search=iphone"
+  );
+  const data = await res.json();
+  const phones = data.data;
+  displayPhones(phones);
+};
+
+const displayPhones = (phones) => {
+  const phoneContainer = document.getElementById("phone-container");
+
+  phones.forEach((phone) => {
+    // create a  div
+    const phoneCard = document.createElement("div");
+    phoneCard.classList = `card w-96 bg-base-100 shadow-xl`;
+    phoneCard.innerHTML = `
+    <figure>
+    <img
+      src="${phone.image}"
+      alt="Shoes"
+    />
+  </figure>
+
+  <div class="card-body">
+    <h2 class="card-title">Shoes!</h2>
+
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+
+  </div>`;
+
+    phoneContainer.appendChild(phoneCard);
+  });
+};
+
+loadPhone();
